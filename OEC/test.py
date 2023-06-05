c77424d85ae7c0e57e596264b2f170086807c286
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-06-05 12:32:36 +0300

    Update test.py

--- OEC/test.py
+++ OEC/test.py
@@ -1,8 +1,26 @@
 import os
 
-my_secret = os.environ.get('TEST')
+def get_cli_args():
+    """
+    Get command line arguments
+    :return: command line argument object
+    """
+    parser = argparse.ArgumentParser()
 
-if my_secret:
-    print(f"The secret value is: {my_secret}")
-else:
-    print("Secret not found.")
+    parser.add_argument(
+        '--test', required=True, dest='TEST', help='test'
+    )
+    return parser.parse_args()
+
+def print_secret(secret1)
+    try:
+        print(secret1)
+    except:
+        print("no secert")
+    
+def main():
+    args = get_cli_args()
+    print_secret(args.TEST)
+
+if __name__ == '__main__':
+    main()
