99a43b2640341de42f917fff60dfb9c8deab22ad
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-06-06 13:09:28 +0300

    Update test.py

--- OEC/test.py
+++ OEC/test.py
@@ -1,5 +1,19 @@
-import subprocess
+import requests
 
-workflow_name = "test1.yml"
+# Perform checks and determine which workflow to trigger
+chosen_workflow = "test1.yml"
 
-subprocess.run(["gh", "workflow", "run", workflow_name], check=True)
+# Trigger the chosen workflow using the GitHub API
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": "Bearer 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"
+}
+repo_owner = "OfirBador"
+repo_name = "testing"
+api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/actions/workflows/{chosen_workflow}/dispatches"
+response = requests.post(api_url, headers=headers)
+
+if response.status_code == 204:
+    print("Workflow run triggered successfully.")
+else:
+    print("Failed to trigger the workflow run.")
