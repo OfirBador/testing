0704d3a0f60608432f0287556651454dc4628b7d
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-06-06 12:06:40 +0300

    Update test2.py

--- OEC/test2.py
+++ OEC/test2.py
@@ -1,7 +1,9 @@
 import sys
 
+print("in script 2")
 if len(sys.argv) > 1:
     secret_value = sys.argv[1]
     print("Secret value:", secret_value)
 else:
     print("Secret not provided.")
+print("out script 2")
