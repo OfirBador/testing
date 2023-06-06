c453d640ffcf3d84a9b8846381ffc6f38556c727
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-06-06 14:37:18 +0300

    Update test1.py

--- OEC/test1.py
+++ OEC/test1.py
@@ -1,4 +1,10 @@
 import sys
 
+if sys.argv[0] == '1234':
+    print(" 0 ok it's works")
+
 if sys.argv[1] == '1234':
-    print("ok it's works")
+    print("1 ok it's works")
+
+if sys.argv[2] == '1234':
+    print("2 ok it's works")
