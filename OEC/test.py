b049202f20878eb858b6a50948dfb9c3c9807ef9
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-06-06 12:06:18 +0300

    Update test.py

--- OEC/test.py
+++ OEC/test.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
 
+print("in script 1")
 secret_value = os.environ.get('SECRET_VALUE')
 
 if secret_value:
     subprocess.run(['python', 'OEC/test2.py', secret_value])
 else:
     print("Secret not found or not set.")
+print("out script 1")
