6ca172b0013945eea289c4abc5850b043052015d
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-07-06 13:19:58 +0300

    Update test.py

--- OEC/another/test.py
+++ OEC/another/test.py
@@ -1,15 +1,16 @@
 import sys
 import os 
 
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+function_dir = os.path.join(parent_dir, 'function')
+sys.path.append(function_dir)
+import fun
 
-from function.fun import *
 
 def main():
-    fun1()
+    fun.fun1()
 
 
 if __name__ == '__main__':
