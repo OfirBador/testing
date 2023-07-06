df485ccfe52ed0ea6bc3a7d7590ddddbbc7a6eaf
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-07-06 12:56:22 +0300

    Update test.py

--- OEC/test.py
+++ OEC/test.py
@@ -1,9 +1,15 @@
-import subprocess
+import sys
 import os 
-import ..function.fun as functions
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from function.fun import *
 
 def main():
-    functions.fun1()
+    fun.fun1()
 
 
 if __name__ == '__main__':
