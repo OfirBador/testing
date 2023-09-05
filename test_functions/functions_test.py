a94ce35a07ce8f2ce045606f32cdd99eeae5660e
OfirBador <111045279+OfirBador@users.noreply.github.com>
2023-09-05 10:07:06 +0300

    Update functions_test.py

--- test_functions/functions_test.py
+++ test_functions/functions_test.py
@@ -1,10 +1,9 @@
 from scripts import script1
-function = script1
-
+from pathlib import Path
 
 def main():
-    function.fun1()
-
+    # function.fun1()
+    script_path = Path(__file__).absolute()
 
 if __name__ == '__main__':
     main()
